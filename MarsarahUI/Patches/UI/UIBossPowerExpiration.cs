73683a9455a4210df8729e8793e36283c5e8213c
Marius Corca <mariuscorca@gmail.com>
2026-09-11 13:44:07 +0200

    Ported boss power expiration message

---
+++ MarsarahUI/Patches/UI/UIBossPowerExpiration.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using MarsarahUI.Managers;
+
+namespace MarsarahUI.Patches.UI
+{
+	internal class UIBossPowerExpiration
+	{
+		private static readonly LogManager log = new LogManager("UI Boss Power Expire", LogManager.LogLevel.Warning);
+
+		[HarmonyPatch(typeof(StatusEffect), nameof(StatusEffect.Stop))]
+		private static class StatusEffectStopPatch
+		{
+			private static void Postfix(StatusEffect __instance)
+			{
+				if (!ConfigManager.EffectiveShowBossExpirationMessage) return;
+
+				Character character = __instance.m_character;
+				if (character == null || !character.IsPlayer() || !character.IsOwner()) return;
+
+				string statusEffectName = __instance.name;
+
+				if (statusEffectName.Contains("GP_"))
+				{
+					string powerName = Localization.instance.Localize(__instance.m_name);
+
+					log.Info($"Forsaken Power expired: {powerName}");
+					character.Message(MessageHud.MessageType.Center, $"{powerName} Power Expired");
+				}
+			}
+		}
+	}
+}
