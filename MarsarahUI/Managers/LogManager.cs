2d15a1365d3ccd3667b53e3d140e0ed7c2b8d01e
Marius Corca <mariuscorca@gmail.com>
2026-09-11 11:40:09 +0200

    Added libs, logger and main cs

---
+++ MarsarahUI/Managers/LogManager.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+namespace MarsarahUI.Managers
+{
+	internal class LogManager
+	{
+		public enum LogLevel
+		{
+			None = 0,
+			Error = 1,
+			Warning = 2,
+			Info = 3
+		}
+
+		private readonly string classLogTitle;
+		private LogLevel classLogLevel;
+		private static LogLevel globalLogLevel = LogLevel.None;
+
+		public LogManager(string logTitle, LogLevel level)
+		{
+			classLogTitle = logTitle;
+			classLogLevel = level;
+		}
+
+		public static void SetGlobalLogLevel(LogLevel level)
+		{
+			globalLogLevel = level;
+		}
+
+		public void SetClassLogLevel(LogLevel level)
+		{
+			classLogLevel = level;
+		}
+
+		private bool ShouldLog(LogLevel messageLevel)
+		{
+			return messageLevel <= classLogLevel && messageLevel <= globalLogLevel;
+		}
+
+		public void Info(string message, bool header = false, bool footer = false)
+		{
+			if (!ShouldLog(LogLevel.Info)) return;
+			if (header) Debug.Log("===================================================");
+			Debug.Log($"[Marsarah UI] [{classLogTitle}] : {message}");
+			if (footer) Debug.Log("===================================================");
+		}
+
+		public void Warn(string message, bool header = false, bool footer = false)
+		{
+			if (!ShouldLog(LogLevel.Warning)) return;
+			if (header) Debug.Log("===================================================");
+			Debug.LogWarning($"[Marsarah UI] [{classLogTitle}] : {message}");
+			if (footer) Debug.Log("===================================================");
+		}
+
+		public void Error(string message, bool header = false, bool footer = false)
+		{
+			if (!ShouldLog(LogLevel.Error)) return;
+			if (header) Debug.Log("===================================================");
+			Debug.LogError($"[Marsarah UI] [{classLogTitle}] : {message}");
+			if (footer) Debug.Log("===================================================");
+		}
+	}
+}
---
+++ MarsarahUI/MarsarahUI.cs
@@ -0,0 +1,27 @@
+﻿using BepInEx;
+using HarmonyLib;
+using MarsarahUI.Managers;
+
+namespace MarsarahUI
+{
+	[BepInPlugin(ModGUID, ModName, ModVersion)]
+	public class MarsarahUI : BaseUnityPlugin
+	{
+		internal const string ModName = "MarsarahUI";
+		internal const string ModVersion = "1.0.0";
+		internal const string Author = "Marsarah";
+		public const string ModGUID = Author + "." + ModName;
+
+		private readonly Harmony harmony = new Harmony(ModGUID);
+		private static readonly LogManager log = new LogManager("Main", LogManager.LogLevel.Info);
+
+		private void Awake()
+		{
+			LogManager.SetGlobalLogLevel(LogManager.LogLevel.Info);
+
+			harmony.PatchAll();
+
+			log.Info($"{ModName} v{ModVersion} loaded.");
+		}
+	}
+}
